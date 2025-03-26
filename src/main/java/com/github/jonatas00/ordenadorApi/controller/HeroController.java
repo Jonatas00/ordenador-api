2487fcbe98ff0f511ce4343b110f68ee851c2fe9
Jonatas <djonatas31@gmail.com>
2025-03-26 12:12:21 -0300

    change controller route name

--- src/main/java/com/github/jonatas00/ordenadorApi/controller/HeroController.java
+++ src/main/java/com/github/jonatas00/ordenadorApi/controller/HeroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("character")
+@RequestMapping("hero")
 public class HeroController {
   private final HeroService heroService;
 
