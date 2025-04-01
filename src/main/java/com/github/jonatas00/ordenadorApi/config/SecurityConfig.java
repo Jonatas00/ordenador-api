3f709c34713e542398372719424687a746f64960
Jonatas00 <djonatas31@gmail.com>
2025-04-01 17:03:51 -0300

    add httpBasic in Security Config

--- src/main/java/com/github/jonatas00/ordenadorApi/config/SecurityConfig.java
+++ src/main/java/com/github/jonatas00/ordenadorApi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.jonatas00.ordenadorApi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,12 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(Customizer.withDefaults())
         .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/swagger-ui/**").permitAll()
             .anyRequest().authenticated()
         );
 
