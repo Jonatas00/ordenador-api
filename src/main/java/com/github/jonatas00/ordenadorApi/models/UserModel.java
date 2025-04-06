213b6c1a3f4741765ee8b3db6df1876cef46fe22
Jonatas00 <djonatas31@gmail.com>
2025-04-06 00:18:07 -0300

    Update Methods for UserModel

--- src/main/java/com/github/jonatas00/ordenadorApi/models/UserModel.java
+++ src/main/java/com/github/jonatas00/ordenadorApi/models/UserModel.java
@@ -5,11 +5,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "tb_user")
 public class UserModel implements UserDetails {
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private UUID id;
@@ -20,18 +20,40 @@ public class UserModel implements UserDetails {
   @Column(nullable = false)
   private String password;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
-  }
 
   @Override
-  public String getPassword() {
-    return "";
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
   }
 
   @Override
   public String getUsername() {
-    return "";
+    return this.username;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
   }
 }
