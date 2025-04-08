026e5b3f95d3b856d4f9bd7887bdd76a2ac15ebc
Jonatas <djonatas31@gmail.com>
2025-04-08 09:57:46 -0300

    🔥 reformat code & add RoleModel

--- src/main/java/com/github/jonatas00/ordenadorApi/OrdenadorApiApplication.java
+++ src/main/java/com/github/jonatas00/ordenadorApi/OrdenadorApiApplication.java
@@ -2,7 +2,6 @@ package com.github.jonatas00.ordenadorApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class OrdenadorApiApplication {
