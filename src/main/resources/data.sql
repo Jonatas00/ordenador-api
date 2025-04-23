b7d814948aebbe9569514d7060887324b0bd5821
Jonatas00 <djonatas31@gmail.com>
2025-04-23 20:01:09 -0300

    🔧 fix data.sql add id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 /*test*/
 
-INSERT INTO tb_role (name)
-values ('ROLE_ADMIN') ON CONFLICT(id) DO NOTHING;
+INSERT INTO tb_role (id, name)
+values (1, 'ROLE_ADMIN') ON CONFLICT(id) DO NOTHING;
 
-INSERT INTO tb_role (name)
-values ('ROLE_USER') ON CONFLICT(id) DO NOTHING;
+INSERT INTO tb_role (id, name)
+values (2, 'ROLE_USER') ON CONFLICT(id) DO NOTHING;
