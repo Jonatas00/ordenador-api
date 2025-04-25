ec2648b40ffc9b33324d2f7fb2fbec7405b4d00b
Jonatas <djonatas31@gmail.com>
2025-04-25 11:28:48 -0300

    🔧 change class to record

--- src/main/java/com/github/jonatas00/ordenadorApi/dto/response/ApiResponse.java
+++ src/main/java/com/github/jonatas00/ordenadorApi/dto/response/ApiResponse.java
@@ -1,19 +1,6 @@
 package com.github.jonatas00.ordenadorApi.dto.response;
 
-public class ApiResponse<T> {
-  private int status;
-  private T body;
-
-  public ApiResponse(int status, T body) {
-    this.status = status;
-    this.body = body;
-  }
-
-  public int getStatus() {
-    return status;
-  }
-
-  public T getBody() {
-    return body;
-  }
-}
+public record ApiResponse<T> (
+  int status,
+  T message
+){}
